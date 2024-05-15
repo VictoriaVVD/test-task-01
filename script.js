af27d473c650fa72daed3748aa47f4b393ce66fd
VictoriaVVD <viktoriya-ku@mail.ru>
2024-05-15 23:06:42 +0300

    Modal window

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
     function toggleHeading() {
-        $( ".heading__wrapper" ).toggle();
+        $(".heading__wrapper").toggle();
     }
-    $( ".btn-1" ).on( "click", toggleHeading );
+    $(".btn-1").on("click", toggleHeading);
+    
     function changeBlocks() {
-        const box = $( ".section__container" );
-        if (box.children().eq(0).hasClass( "b-dashed" )) {
-            box.find( box.children().eq(0) ).removeClass( "b-dashed" )
-            box.find( box.children().eq(1) ).addClass( "b-dashed" )
+        const box = $(".section__container");
+        if (box.children().eq(0).hasClass("b-dashed")) {
+            box.find(box.children().eq(0)).removeClass("b-dashed");
+            box.find(box.children().eq(1)).addClass("b-dashed" );
         }
         else {
-            box.find( box.children().eq(0) ).addClass( "b-dashed" )
-            box.find( box.children().eq(1) ).removeClass( "b-dashed" )
+            box.find(box.children().eq(0)).addClass("b-dashed");
+            box.find(box.children().eq(1)).removeClass("b-dashed");
         }
     }
-    $( ".btn-2" ).on( "click", changeBlocks );
+    $(".btn-2").on("click", changeBlocks);
+
+
+    const modal = $(".modal__container");
+    const closeBtn = $(".btn_close");
+    function closeWindow() {
+        modal.hide();
+    }
+    closeBtn.on( "click", closeWindow);
+
 })
